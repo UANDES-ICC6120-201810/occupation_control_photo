19f6ebc9c38affe3e0a8e8093cecd23571bd07f4
Diego Beckdorf <dbeckdorf@miuandes.cl>
2018-05-30 08:02:43 +0000

    fix setup and run scripts for static local folders

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-parent_folder=$( cd "$(dirname "${BASH_SOURCE[0]}")" ;pwd -P )
-cd "$parent_folder"
 
-echo $parent_folder
-
-sudo python main.py rtsp://192.168.1.190:554
+sudo python /usr/local/bin/occupation/main.py rtsp://192.168.1.190:554
--- setup.sh
+++ setup.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-parent_folder=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+source_folder=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+directory="/usr/local/bin/occupation_module"
 
 # Setup for stop bus occupation module of ZAPO proyect
-
 echo Installing module dependencies...
 sudo apt-get install python python-pip || echo "Installation Failed" exit
 sudo pip install numpy || echo "Python requirements could not be installed" exit
 sudo apt-get install python-opencv || echo "OpeCv for python could not be installed" exit
 echo Requirements installed successfuly
 
-echo Creting required folders and files
-cd "$parent_folder"
+echo Creting required folders ...
+sudo mkdir $directory
+cd $directory
+echo Copying files to folder ...
+sudo cp $source_folder/run.sh $directory/run.sh
+sudo cp $source_folder/main.py $directory/main.py
 echo creating registers.log ...
-touch registers.log
+sudo touch registers.log
 echo Creating images folder ...
-mkdir ../img_folder
+sudo mkdir img_folder
 echo required folders and files created
 
 echo Setting cron job default every 5 minutes
-sudo crontab -l | sed "/run.sh/ c*/5 * * * * sudo sh $parent_folder/run.sh > $parent_folder/registers.log 2>&1" | sudo crontab -
+sudo crontab -l | sed "/run.sh/ c*/5 * * * * sudo sh $directory/run.sh > $directory/registers.log 2>&1" | sudo crontab -
 sudo service cron reload
 sudo service cron restart
 echo Jos setted up correctly
