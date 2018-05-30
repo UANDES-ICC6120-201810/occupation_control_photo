fef2fdcc9bbef71c2847e3d511b7492aefb2871e
Diego Beckdorf <dbeckdorf@miuandes.cl>
2018-05-30 05:06:17 +0000

    modify run folder path and setup crontab

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-parent_folder=$( cd "$dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+parent_folder=$( cd "$(dirname "${BASH_SOURCE[0]}")" ;pwd -P )
 cd "$parent_folder"
+
+echo $parent_folder
 
 sudo python main.py rtsp://192.168.1.190:554
--- setup.sh
+++ setup.sh
@@ -11,13 +11,14 @@ echo Requirements installed successfuly
 
 echo Creting required folders and files
 cd "$parent_folder"
+echo creating registers.log ...
 touch registers.log
-touch /var/log/picture.log
+echo Creating images folder ...
 mkdir ../img_folder
 echo required folders and files created
 
 echo Setting cron job default every 5 minutes
-(sudo crontab -l 2>/dev/null; echo "*/5 * * * * sudo sh "$parent_folder"/run.sh > "$parent_folder"/registers.log 2>&1") | sudo crontab -
+sudo crontab -l | sed "/run.sh/ c*/5 * * * * sudo sh $parent_folder/run.sh > $parent_folder/registers.log 2>&1" | sudo crontab -
 sudo service cron reload
 sudo service cron restart
 echo Jos setted up correctly
